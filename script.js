59505768787d9d5e6ec714a45018050256c1927c
Abel Alonso <abel.alonso@gmail.com>
2018-07-16 22:21:46 +0200

    formulario completo

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 window.onload = function(){
 
-  let counterStep=1;
-  let counterIngredient=1;
+  let counterStep = 1;
+  let counterIngredient = 1;
+  let counterKeyword = 1;
 
   $('#addIngredient').click(function(ev) {
     ev.preventDefault();
     if (($(`#quantity${counterIngredient-1}`).val()!="") &&
         ($(`#ingredient${counterIngredient-1}`).val()!="")){
+      console.log($(`#quantity${counterIngredient-1}`).parent().val());
       $(this).before(`<div class="ingredient">
                       <input class="quantity${counterIngredient}" type="text" placeholder="Cantidad">
                       <input class="ingredient${counterIngredient}" type="text" placeholder="Ingrediente">
@@ -19,6 +21,15 @@ window.onload = function(){
     if ($(`#step${counterStep-1}`).val() !=""){
       $(this).before(`<div class='step'>
                         <textarea id="step${counterStep}" placeholder="Paso ${++counterStep}" rows="5"></textarea>
+                      </div>`);
+    }
+  });
+
+  $('#addKeyword').click(function(ev) {
+    ev.preventDefault();
+    if ($(`#keyword${counterKeyword-1}`).val() !=""){
+      $(this).before(`<div class='step'>
+                        <input id="step${counterKeyword}" placeholder="Palabra Clave">
                       </div>`);
     }
   });
